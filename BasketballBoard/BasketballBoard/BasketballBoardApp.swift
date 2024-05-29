62b95b1e8563afb339f657c82efbcbb50826362f
Yehor Nechvola <ynechvola@eva.ua>
2024-05-29 18:27:08 +0300

    Created empty project

---
+++ BasketballBoard/BasketballBoard/BasketballBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballBoardApp.swift
+//  BasketballBoard
+//
+//  Created by Eva on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BasketballBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
